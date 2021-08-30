04b3e55a0b46a75b5f99b9a6f377bccfd7ce838d
hicey <hiceycook@gmail.com>
2021-08-30 03:20:24 -0400

    finished enemy tests and object

--- __tests__/Enemy.test.js
+++ __tests__/Enemy.test.js
@@ -1,4 +1,5 @@
 const { test, expect } = require('@jest/globals');
+const { stringContaining } = require('expect');
 const Enemy = require('../lib/Enemy.js');
 const Potion = require('../lib/Potion.js');
 
@@ -14,3 +15,46 @@ test('create an enemy object', () => {
     expect(enemy.agility).toEqual(expect.any(Number));
     expect(enemy.potion).toEqual(expect.any(Object));
 });
+
+test("gets enemy's health value", () => {
+    const enemy = new Enemy('goblin', 'sword');
+
+    expect(enemy.getHealth()).toEqual(expect.stringContaining(enemy.health.toString()));
+});
+
+test('checks if enemy is alive or not', () => {
+    const enemy = new Enemy('goblin', 'sword');
+
+    expect(enemy.isAlive()).toBeTruthy();
+
+    enemy.health = 0;
+
+    expect(enemy.isAlive()).toBeFalsy();
+});
+
+test("gets enemy's attack value", () => {
+    const enemy = new Enemy('goblin', 'sword');
+    enemy.strength = 10;
+
+    expect(enemy.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(enemy.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+test("check if enemy health reduces", () => {
+    const enemy = new Enemy('goblin', 'sword');
+    const oldHealth = enemy.health;
+
+    enemy.reduceHealth(5);
+
+    expect(enemy.health).toBe(oldHealth - 5);
+
+    enemy.reduceHealth(99999);
+
+    expect(enemy.health).toBe(0);
+});
+
+test('get description of enemy', () => {
+    const enemy = new Enemy('goblin', 'sword');
+    expect(enemy.getDescription()).toEqual(expect.stringContaining('goblin'));
+    expect(enemy.getDescription()).toEqual(expect.stringContaining('sword'));
+})
